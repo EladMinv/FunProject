63b5c821864417795b96c77620c83d119b20701c
eladWorker1 <eladworkew@gmail.com>
2026-07-03 14:20:16 +0300

    Adding Flask and update .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from flask import Flask, jsonify, json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return { 'message': 'Hello World!' }, 200
+
+@app.route('/about')
+def health():
+    return jsonify({'status: ok'}), 200
+    # return 'OK'
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
